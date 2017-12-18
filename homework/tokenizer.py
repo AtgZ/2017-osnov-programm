50d8b227528460787332757196f3d427f74a4612
AtgZ <atgz@mail.ru>
2017-12-18 13:02:26 +0100

    add hw2

---
+++ homework/tokenizer.py
@@ -0,0 +1,18 @@
+import sys
+text = sys.stdin.read()
+for i in ',:;?!.—)':
+	text = text.replace(i, ' '+i)
+for i in '(—.':
+	text = text.replace(i, i+' ')
+for i in '1234567890': 
+	for j in '1234567890':
+		text = text.replace(i + ' ' + j, i + j)	
+row = text.split(' ')
+
+
+token_id = 1
+for token in row:
+	token= token.strip()
+	if token!='':
+		print('%d\t%s\t_\t_\t_\t_\t_\t_\t_\t_' % (token_id, token))
+	token_id=token_id+1
